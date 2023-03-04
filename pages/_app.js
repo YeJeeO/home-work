cc7db5adc7303b81c2ddc27633bbbd3594bb2674
YeJeeO <yejeeo@yandex.ru>
2023-03-05 00:29:58 +0700

    +pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import TodoList from '../components/TodoList';
+import TodoForm from '../components/TodoForm';
+import "../styles/style.css";
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+  
+  export default MyApp
